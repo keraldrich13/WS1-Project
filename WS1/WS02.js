e506bc326b2111c8c6a8a7d7f7bf2ac13084214e
keraldrich13 <keraldrich@gmail.com>
2024-10-31 18:47:19 +0800

    Update WS02.js

--- WS1/WS02.js
+++ WS1/WS02.js
@@ -6,7 +6,24 @@ cartBtn.onclick = () => cartModal.style.display = 'block';
 closeCart.onclick = () => cartModal.style.display = 'none';
 
 window.onclick = (event) => {
-     if (event.target === cartModal) {
-         cartModal.style.display = 'none';
-            }
-        };
+    if (event.target === cartModal) {
+        cartModal.style.display = 'none';
+        }
+    };
+
+const scrollRevealOption = {
+     distance: "50px",
+     origin: "bottom",
+    duration: 1000,
+};
+
+ScrollReveal().reveal(".header_image img", {
+    ...scrollRevealOption,
+    origin: "right",
+});
+ScrollReveal().reveal(".header_content h1", {
+    ...scrollRevealOption,
+    delay:500,
+});
+
+
